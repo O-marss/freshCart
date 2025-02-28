d4a385434398e0341358b1d0e2271c5c5620e715
0mar-cc <omarmoh3010@gmail.com>
2025-02-28 18:17:27 +0200

    Updates

--- assets/useBrands-Bl34rzI0.js
+++
@@ -1,1 +0,0 @@
-import{P as n,H as a}from"./index-C0o9T5AH.js";function d(e,r){function s(){return a.get(e)}return n({queryKey:[r],queryFn:s,select:t=>t.data.data})}export{d as u};
---
+++ assets/useBrands-CtqKnrEH.js
@@ -0,0 +1,1 @@
+import{P as n,H as a}from"./index-CqTmC2BM.js";function d(e,r){function s(){return a.get(e)}return n({queryKey:[r],queryFn:s,select:t=>t.data.data})}export{d as u};
